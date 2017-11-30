b681c869a5805b6bfb2bb75389b17ab94958f962
Linus Ringheim <linus.ringheim@hotmail.com>
2017-11-30 14:59:02 +0100

    Created Gui + Gui Code

---
+++ Laboration-2/Laboration-2/GuiGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Laboration_2
+{
+    class GuiGenerator
+    {
+        public void GenerateListBoxScore(int selecstionIndex, AllData allData, ref ListBox inputListBox)
+        {
+            if (allData.gameData[selecstionIndex].levelList.Count != 0)
+            {
+                if (selecstionIndex != -1)
+                {
+
+                    int nummberOfMovesDone = 0;
+                    int nummberOfMovesAccepted = 0;
+
+                    inputListBox.Items.Clear();
+                    Player tempPlayer = allData.gameData[selecstionIndex];
+
+                    for (int i = 0; i != tempPlayer.levelList.Count; i++)
+                    {
+
+                        ItemsControl tempItemControlLevelData = new ItemsControl();
+
+                        tempItemControlLevelData.Height = 70;
+                        tempItemControlLevelData.Width = 135;
+
+                        Label tempInfoLabelData = new Label();
+
+                        nummberOfMovesDone += tempPlayer.scoreList[i].PlayerSore;
+                        nummberOfMovesAccepted += tempPlayer.levelList[i].MaxMoves;
+
+                        tempInfoLabelData.Content = "Level : " + tempPlayer.levelList[i].Name + "\r\n";
+                        tempInfoLabelData.Content += "Moves made : " + tempPlayer.scoreList[i].PlayerSore + "\r\n";
+                        tempInfoLabelData.Content += "Moves left : " + (tempPlayer.levelList[i].MaxMoves - tempPlayer.scoreList[i].PlayerSore) + "\r\n";
+                        tempInfoLabelData.Content += "Moves available : " + tempPlayer.levelList[i].MaxMoves + "\r\n";
+                        tempInfoLabelData.Content += "-----------------";
+
+
+                        tempItemControlLevelData.Items.Add(tempInfoLabelData);
+                        inputListBox.Items.Add(tempItemControlLevelData);
+
+                    }
+
+                    ItemsControl tempItemControlStats = new ItemsControl();
+
+                    tempItemControlStats.Height = 70;
+                    tempItemControlStats.Width = 150;
+
+                    Label tempInfoLabel = new Label();
+
+
+                    tempInfoLabel.Content += "Total moves made : " + nummberOfMovesDone + "\r\n";
+                    tempInfoLabel.Content += "Total moves left : " + (nummberOfMovesAccepted - nummberOfMovesDone) + "\r\n";
+                    tempInfoLabel.Content += "Total moves available : " + nummberOfMovesAccepted;
+
+
+                    tempItemControlStats.Items.Add(tempInfoLabel);
+                    inputListBox.Items.Add(tempItemControlStats);
+
+                }
+            }
+            else
+            {
+                inputListBox.Items.Clear();
+            }
+
+        }
+    }
+}
--- Laboration-2/Laboration-2/MainWindow.xaml.cs
+++ Laboration-2/Laboration-2/MainWindow.xaml.cs
@@ -20,9 +20,81 @@ namespace Laboration_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        AllData allData = new AllData();
+        GuiGenerator guiGenerator = new GuiGenerator();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            allData.gameData.Add(new Player("Linus"));
+            allData.gameData.Add(new Player("André"));
+
+            allData.gameData[0].AddNewScore(new Score(10), new Level("Level 1", 10));
+            allData.gameData[0].AddNewScore(new Score(6), new Level("Level 2", 6));
+
+            allData.gameData[1].AddNewScore(new Score(5), new Level("Level 1", 10));
+            allData.gameData[1].AddNewScore(new Score(3), new Level("Level 2", 6));
+
+            LoadPlayerListBox();
+        }
+
+        private void LoadPlayerListBox()
+        {
+            for (int i = 0; i != allData.gameData.Count; i++)
+            {
+
+                ListBoxPlayer.Items.Add(allData.gameData[i].Name);
+            }
+
+        }
+
+        private void ListBoxPlayer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            guiGenerator.GenerateListBoxScore(ListBoxPlayer.SelectedIndex, allData, ref ListBoxScore);
+        }
+
+
+
+        private void TextBoxName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+
+                if (TextBoxName.Text.Length != 0)
+                {
+
+
+                    int nameIndex = allData.gameData.FindIndex(x => x.Name.ToLower() == TextBoxName.Text.ToLower());
+
+                    if (nameIndex != -1)
+                    {
+
+                        guiGenerator.GenerateListBoxScore(nameIndex, allData, ref ListBoxScore);
+                    }
+                    else
+                    {
+                        allData.gameData.Add(new Player(TextBoxName.Text));
+                        ListBoxPlayer.Items.Add(TextBoxName.Text);
+                        ListBoxScore.Items.Clear();
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Du angav inget namn!");
+                }
+
+
+            }
+
+            
+        }
+
+        private void ListBoxScore_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
+
